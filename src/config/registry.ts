057a6e10429afcf776ae455b75b66eabee0ff86c
Santosh145 <santoshmahapatro@iserveu.co.in>
2026-03-24 21:12:19 +0530

    added vercel package

--- src/config/registry.ts
+++ src/config/registry.ts
@@ -1,4 +1,4 @@
 export const registryConfig = {
   /** Used to replace the <registryBaseUrl> placeholder in dependency paths */
-  baseUrl: process.env.REGISTRY_URL || "https://chandnapradhan.vercel.app",
+  baseUrl: process.env.REGISTRY_URL || "https://https://santoshsportfolio.vercel.app",
 };
